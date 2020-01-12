759b935c5a2f0e8f710206521e9d4469dcc65768
TRO-NOMIOS <you@example.com>
2020-01-12 20:38:06 +0100

    test

--- filter_plugins/monitors_list.py
+++ filter_plugins/monitors_list.py
@@ -7,8 +7,11 @@ class FilterModule(object):
             'monitors_list': self.monitors_list
         }
 
-    def monitors_list(self, dummy_variable, http_monitor_select, https_monitor_select, vs_name):
+    def monitors_list(self, dummy_variable, tcp_monitor_select, tcp_monitor_port, \
+            http_monitor_select, https_monitor_select, vs_name):
         monitors_list = []
+        if tcp_monitor_select == 'ENABLED':
+            monitors_list.append('tcp_monitor_' + str(tcp_monitor_port))
         if http_monitor_select == 'ENABLED':
             monitors_list.append(vs_name + '_http_monitor')
         elif https_monitor_select == 'ENABLED':
