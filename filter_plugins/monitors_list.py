7e0b6b07ba9242f2259ee24021b5b7c6b74ac754
TRO-NOMIOS <you@example.com>
2020-01-12 18:35:29 +0100

    trst

--- filter_plugins/monitors_list.py
+++ filter_plugins/monitors_list.py
@@ -10,17 +10,8 @@ class FilterModule(object):
     def monitors_list(self, dummy_variable, http_monitor_select, https_monitor_select, vs_name):
         monitors_list = []
         if http_monitor_select == 'ENABLED':
-            http_monitor = {'http_monitor_enabled': True, 'http_monitor_name': vs_name + '_http_monitor'}
-        else:
-            http_monitor = {'http_monitor_enabled': False, 'http_monitor_name': None}
-
-        monitors_list.append(http_monitor)
-
-        if https_monitor_select == 'ENABLED':
-            https_monitor = {'https_monitor_enabled': True, 'https_monitor_name': vs_name + '_https_monitor'}
-        else:
-            https_monitor = {'https_monitor_enabled': False, 'https_monitor_name': None}
-
-        monitors_list.append(https_monitor)   
+            monitors_list.append(vs_name + '_http_monitor')
+        elif https_monitor_select == 'ENABLED':
+            monitors_list.append(vs_name + '_http_monitor')
 
         return monitors_list
